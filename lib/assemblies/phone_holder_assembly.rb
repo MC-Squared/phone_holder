cc20b4af9179a0d92d54dc51ad23b520d37c6174
J Miles <j.milesy.nz@gmail.com>
2017-11-15 17:58:46 +1300

    Rotate phone assembly to be z-up

--- lib/assemblies/phone_holder_assembly.rb
+++ lib/assemblies/phone_holder_assembly.rb
@@ -7,17 +7,22 @@ class PhoneHolderAssembly < SolidRuby::Assembly
   skip :output
 
   def part(show)
-    res = Phone.new.debug.translate(z: 3)
+    res = Phone.new
+      .rotate(x: 90)
+      .translate(z: 5)
 
     res += BraceAssembly.new
-      .translate(x: -5, y: $phone_h*0.275)
+      .rotate(x: 90)
+      .translate(x: -5, y: 3, z: $phone_h/3.0)
 
     res += BraceAssembly.new
+      .rotate(x: 90)
       .mirror(x: 1)
-      .translate(x: $phone_w + 5, y: $phone_h*0.275)
+      .translate(x: $phone_w + 5, y: 3, z: $phone_h/3.0)
 
     res += Wishbone.new
       .rotate(z: 180)
-      .translate(x: $phone_w/2.0, y: $phone_h/2.5, z: -10)
+      .rotate(x: 90)
+      .translate(x: $phone_w/2.0, y: 8, z: $phone_h/2.0 - 5)
   end
 end
