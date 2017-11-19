9b25a323ddb2f98c95186dd3e3e45aa3b2b93772
J Miles <j.milesy.nz@gmail.com>
2017-11-19 21:36:05 +1300

    More work on vent mount

--- lib/printed/vent_mount.rb
+++ lib/printed/vent_mount.rb
@@ -7,21 +7,37 @@ class VentMount < SolidRuby::Printed
   def part(_show)
     res = cylinder(d: $pin_d, h: 50)
 
+    res += Pin.new.show
+      .translate(z: 50)
+
     # cut cylinder into one third
-    res -= triangle(a: $pin_d/2.0, alpha: 60, beta: 90)
-      .translate(x: -$pin_d/2.0)
-      .linear_extrude(h: 50 + 1)
+    res -= triangle(a: $pin_d, alpha: 60, beta: 90)
+      .translate(x: -$pin_d)
+      .linear_extrude(h: 50 + $pin_h*2.0 + 1)
       .translate(z: -0.5)
 
-    res -= triangle(a: $pin_d/2.0, alpha: 60, beta: 90)
+    res -= triangle(a: $pin_d, alpha: 60, beta: 90)
       .mirror(x: 1)
-      .translate(x: $pin_d/2.0)
-      .linear_extrude(h: 50 + 1)
+      .translate(x: $pin_d)
+      .linear_extrude(h: 50 + $pin_h*2.0 + 1)
       .translate(z: -0.5)
 
-    res -= cube($pin_d, $pin_d, 51)
+    res -= cube($pin_d + 2, $pin_d, 50 + $pin_h*2.0 + 1)
       .center_x
       .translate(y: -$pin_d + 0.01, z: -0.5)
 
+    res += cube($pin_d/2.0, $pin_d, 2)
+      .center_x
+      .fillet(r: 1, edges: :vertical)
+      .translate(y: 1)
+
+    res += cylinder(d: 1, h: 50 + $pin_h + 1.5)
+      .translate(x: 1.5, y: 0.5)
+
+    res -= cylinder(d: 1 + $tolerance/2.0, h: 50 + $pin_h + 2.5)
+      .translate(x: 1.5, y: 0.5, z: -0.5)
+      .rotate(z: 120)
+
+    res
   end
 end
