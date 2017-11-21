74f77fa7b69b2dcc2ff8a5c5414009bb9951c133
J Miles <j.milesy.nz@gmail.com>
2017-11-21 16:45:52 +1300

    Fix phone holder assembly after other changes, add vent mount

--- lib/assemblies/phone_holder_assembly.rb
+++ lib/assemblies/phone_holder_assembly.rb
@@ -9,20 +9,30 @@ class PhoneHolderAssembly < SolidRuby::Assembly
   def part(show)
     res = Phone.new
       .rotate(x: 90)
-      .translate(z: 5)
+      .translate(y: 32, z: 5)
 
     res += BraceAssembly.new.show
       .rotate(x: 90)
-      .translate(x: -5, y: 3, z: $phone_h/3.0)
+      .translate(x: -5, y: 20, z: $phone_h/3.0)
 
     res += BraceAssembly.new.show
       .rotate(x: 90)
       .mirror(x: 1)
-      .translate(x: $phone_w + 5, y: 3, z: $phone_h/3.0)
+      .translate(x: $phone_w + 5, y: 20, z: $phone_h/3.0)
 
     res += Wishbone.new
-      .rotate(z: 180)
-      .rotate(x: 90)
-      .translate(x: $phone_w/2.0, y: 8, z: $phone_h/2.0 - 5)
+      .rotate(x: -90)
+      .translate(x: $phone_w/2.0, y: 10, z: $phone_h/1.75)
+
+    v = VentMount.new.show
+
+    v += VentMount.new.show.rotate(z: 120)
+
+    v += VentMount.new.show.rotate(z: -120)
+
+    v.rotate(x: -90)
+      .translate(x: $phone_w/2.0, y: -35, z: $phone_h/1.75)
+
+    res += v
   end
 end
